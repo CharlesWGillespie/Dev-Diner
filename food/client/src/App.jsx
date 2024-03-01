32f197bf19ab3d97ad13c69ace3d91ec3646b604
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-02-29 21:21:14 -0500

    added signup and signin pages going to be working on homepage

--- food/client/src/App.jsx
+++ food/client/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SignUp from './pages/SignUp'
+import SignUp from './pages/SignUpPage'
+import SignInSide from './pages/SignInPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <SignUp/>
+      <SignInSide/>
     </>
   )
 }
