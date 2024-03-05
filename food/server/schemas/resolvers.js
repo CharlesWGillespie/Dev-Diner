ff9addba3a6a78ef46ed6724eb3adfab6e10aed9
christian goddu <rgoddu909@gmail.com>
2024-03-05 18:54:58 -0500

    user routes

--- food/server/schemas/resolvers.js
+++ food/server/schemas/resolvers.js
@@ -1,16 +1,19 @@
 const { User, Order, menuItem, Category } = require('../models')
 
 const resolvers = {
-Query: {
-    users: async() => {
-        return User.find()
-    }
-},
-Mutation: {
-    addUser: async(parent, {firstName, lastName, email, password, phoneNumber}) => {
-        const user = await User.create({firstName, lastName, email, password, phoneNumber})
-        // const token = signToken(user)
-        return{user}
+    Query: {
+        users: async () => {
+            return await User.find({})
+        }
+    },
+    Mutation: {
+        addUser: async (parent, { firstName, lastName, email, password, phoneNumber }) => {
+            const user = await User.create({ firstName, lastName, email, password, phoneNumber })
+            const token = signToken(user)
+            return { user, token }
+        },
+        login: async ()
     }
 }
-}
+
+module.exports = resolvers
