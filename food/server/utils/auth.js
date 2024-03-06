14d2d4758c92b10b4134d757599163c0b03811aa
christian goddu <rgoddu909@gmail.com>
2024-03-05 19:16:34 -0500

    login mutation

--- food/server/utils/auth.js
+++ food/server/utils/auth.js
@@ -6,33 +6,33 @@ const secret = process.env.SECRET
 const expiration = '8h'
 
 module.exports = {
-    AuthenticationError: new GraphQLError('Could not authenticate user.', {
-      extensions: {
-        code: 'UNAUTHENTICATED',
-      },
-    }),
-    authMiddleware: function ({ req }) {
-        let token = req.body.token || req.query.token || req.headers.authorization;
-    
-        if (req.headers.authorization) {
-          token = token.split(' ').pop().trim();
-        }
-    
-        if (!token) {
-          return req;
-        }
-    
-        try {
-          const { authenticatedPerson } = jwt.verify(token, secret, { maxAge: expiration });
-          req.user = authenticatedPerson;
-        } catch {
-          console.log('Invalid token');
-        }
-    
-        return req;
-      },
-      signToken: function ({ email, firstName, lastName, _id }) {
-        const payload = { email, firstName, lastName, _id };
-        return jwt.sign({ authenticatedPerson: payload }, secret, { expiresIn: expiration });
-      },
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { authenticatedPerson } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = authenticatedPerson;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, firstName, lastName, _id }) {
+    const payload = { email, firstName, lastName, _id };
+    return jwt.sign({ authenticatedPerson: payload }, secret, { expiresIn: expiration });
+  },
 }
