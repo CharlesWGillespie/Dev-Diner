948793dd9007591c3c7898196c6ee8dfe3995f54
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-03-05 20:53:01 -0500

    started styling for card

---
+++ food/client/src/components/Menu-Item.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+
+const MenuItemCard = ({ name, description, price, imageUrl }) => {
+  return (
+    <Card sx={{ maxWidth: 250 }}>
+      <CardMedia
+        component="img"
+        height="250"
+        image={imageUrl}
+        alt={name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          ${price}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuItemCard;
